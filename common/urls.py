3c90a4c8fc7c1dba109f6a7512e301f5c3b7df75
Bbillyben <bbillyben@hotmail.com>
2023-02-20 15:39:18 +0100

    add favorite index plus favorite menu

--- common/urls.py
+++ common/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('nav/', views.get_nav_favorites, name='nav_favorites'),
+    path('nav/accordion', views.get_nav_favorites_accordion, name='nav_favorites_accordion'),
     path('fav-toggle/', views.toggle_favorites, name='favorite_toggle'), 
     path('fav-star/', views.get_user_fav_obj, name='favorite_star'), 
 ]
--- common/views.py
+++ common/views.py
@@ -15,11 +15,32 @@ def get_user_fav_obj(request):
     fav=favorite.objects.filter(user=request.user, content_type=ct, object_id=pk)
     return render(request, 'favorite_star.html', {"fav":fav})
 
-def get_nav_favorites(request):
+
+def get_user_favorite(request):
     fav=favorite.objects.filter(user=request.user).order_by("content_type")
     
-    data={'favorites':FavoriteSerialize(fav, many=True).data}
-    return render(request, 'favorite_nav.html', data)
+    data={} #'favorites':FavoriteSerialize(fav, many=True).data}
+    
+    for el in fav:
+        if not str(el.content_type.model) in data:
+            data[str(el.content_type.model)]=[]
+        data[str(el.content_type.model)].append(el)
+        
+    for els in data:
+          data[els]=FavoriteSerialize(data[els], many=True).data
+    
+    return data
+
+def get_nav_favorites(request):
+    
+    data=get_user_favorite(request)
+    return render(request, 'favorite_nav.html', {"datas":data})
+
+
+def get_nav_favorites_accordion(request):
+    
+    data=get_user_favorite(request)
+    return render(request, 'favorite_accordion.html', {"datas":data})
 
 def toggle_favorites(request):
     if request.method != 'POST':
