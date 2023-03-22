4084069c33624c1e4c76ca3e4ccfde99556cfaa3
Bbillyben <bbillyben@hotmail.com>
2023-03-22 12:15:17 +0100

    setting list first

--- settings/views.py
+++ settings/views.py
@@ -6,8 +6,14 @@ from view_breadcrumbs import BaseBreadcrumbMixin
 
 from .models import LMUserSetting
 
+from fund.models import Cost_Type
+
 class SettingsView(LoginRequiredMixin, BaseBreadcrumbMixin, TemplateView):
     template_name = 'settings/settings.html'
     home_label = '<i class="fas fa-bars"></i>'
     model = LMUserSetting
     crumbs = [("Settings","settings")]
+    
+class SettingList_cost_type(LoginRequiredMixin, TemplateView):
+    template_name = 'settings/setting_table.html'
+    model = Cost_Type
