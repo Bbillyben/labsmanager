ccc4f93f72dbc868f6cef74d5af55fe3ca9fdb69
Bbillyben <bbillyben@hotmail.com>
2023-04-07 12:24:41 +0200

    bug fix budget finder

--- project/models.py
+++ project/models.py
@@ -105,6 +105,7 @@ class Project(ActiveDateMixin):
     def contribution_amount(self):
         from fund.models import Contribution
         return Contribution.objects.filter(fund__project=self.pk).aggregate(Sum('amount'))["amount__sum"]
+    
     @classmethod
     def staleFilter(cls):
         monthToGo=LMUserSetting.get_setting('DASHBOARD_PROJECT_STALE_TO_MONTH')
